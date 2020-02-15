4395afe6b5889a89f34e9eb8e1765d1024186f1a
Heiner Bunjes <heiner@bunjes.net>
2020-02-15 23:57:43 +0100

    limited the log size

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,11 +5,13 @@ echo "#### run.sh has started."
 
 docker rm -f socat
 
-docker run             \
-  -d                   \
-  -p 514:514/udp       \
-  --name socat         \
-  --restart always     \
+docker run               \
+  -d                     \
+  -p 514:514/udp         \
+  --log-opt max-size=10m \
+  --log-opt max-file=5   \
+  --name socat           \
+  --restart always       \
   sejnub/socat:latest
 
 echo "#### run.sh has started."
